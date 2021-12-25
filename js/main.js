3099ab9def69e8e6302e112876a12c44bae584d7
Michael <53647066+TMichaelan@users.noreply.github.com>
2021-12-25 16:08:51 +0100

    click outside the block

--- js/main.js
+++ js/main.js
@@ -1,61 +1,73 @@
 const toggleOptionsList = () => {
+  const mainBlock = document.querySelector(".multibutton-block");
+  const caption = mainBlock.querySelector(".multibutton-caption");
+  const list = mainBlock.querySelector(".multibutton-list-block");
+  const triggerIcon = mainBlock.querySelector(".multibutton__button img");
+  const chatBot = mainBlock.querySelector(".options-chat");
+  const createOrderBlock = mainBlock.querySelector(".create-order");
+  const contactsBlock = mainBlock.querySelector(".contacts-block");
+  const modals = mainBlock.querySelectorAll(".multibutton-modal");
 
-    const mainBlock = document.querySelector('.multibutton-block');
-    const caption = mainBlock.querySelector('.multibutton-caption');
-    const list = mainBlock.querySelector('.multibutton-list-block');
-    const triggerIcon = mainBlock.querySelector('.multibutton__button img');
-    const chatBot = mainBlock.querySelector('.options-chat');
-    const createOrderBlock = mainBlock.querySelector('.create-order');
-    const contactsBlock = mainBlock.querySelector('.contacts-block');
+  mainBlock.addEventListener("click", (e) => {
+    if (e.target.closest(".multibutton__button")) {
+      caption.classList.toggle("hide");
+      list.classList.toggle("active");
 
-    const modals = mainBlock.querySelectorAll('.multibutton-modal');
+      if (list.classList.contains("active")) {
+        triggerIcon.setAttribute("src", "images/icons/dots.svg");
+      } else {
+        triggerIcon.setAttribute("src", "images/options-btn-icon.svg");
+      }
 
-    mainBlock.addEventListener('click', (e) => {
+      modals.forEach((el) => {
+        el.classList.remove("active");
+      });
 
-        if (e.target.closest('.multibutton__button')) {
-            caption.classList.toggle('hide');
-            list.classList.toggle('active');
+      if (list.classList.contains("active")) {
+        caption.classList.add("hide");
+      }
+    }
 
-            if (list.classList.contains('active')) {
-                triggerIcon.setAttribute('src', 'images/icons/dots.svg');
-            } else {
-                triggerIcon.setAttribute('src', 'images/options-btn-icon.svg');
-            };
+    if (e.target.classList.contains("open-chat")) {
+      chatBot.classList.add("active");
+      list.classList.remove("active");
+    }
 
-            modals.forEach(el => {
-                el.classList.remove('active');
-            });
+    if (e.target.classList.contains("order-form")) {
+      createOrderBlock.classList.add("active");
+      list.classList.remove("active");
+    }
 
-            if (list.classList.contains('active')) {
-                caption.classList.add('hide');
-            }
-        };
+    if (e.target.classList.contains("contacts")) {
+      contactsBlock.classList.add("active");
+      list.classList.remove("active");
+    }
 
-        if (e.target.classList.contains('open-chat')) {
-            chatBot.classList.add('active');
-            list.classList.remove('active');
-        };
+    if (e.target.closest(".modal-top__button")) {
+      modals.forEach((el) => {
+        el.classList.remove("active");
+        triggerIcon.setAttribute("src", "images/options-btn-icon.svg");
+        caption.classList.remove("hide");
+      });
+    }
 
-        if (e.target.classList.contains('order-form')) {
-            createOrderBlock.classList.add('active');
-            list.classList.remove('active');
-        };
+    if (list.classList.contains("active")) {
+      addEventListener("click", (e) => {
+        if (!e.target.closest(".multibutton-block")) {
+          modals.forEach((el) => {
+            el.classList.remove("active");
+          });
 
-        if (e.target.classList.contains('contacts')) {
-            contactsBlock.classList.add('active');
-            list.classList.remove('active');
-        };
+          list.classList.remove("active");
+          triggerIcon.setAttribute("src", "images/options-btn-icon.svg");
+        }
+      });
+    }
 
-        if (e.target.closest('.modal-top__button')) {
-            modals.forEach(el => {
-                el.classList.remove('active');
-                triggerIcon.setAttribute('src', 'images/options-btn-icon.svg');
-                caption.classList.remove('hide');
-            });
-        };
-
-    });
+    if (e.target.closest(".options-chat__form-btn")) {
+      console.log("SENDING");
+    }
+  });
 };
 
 toggleOptionsList();
-
